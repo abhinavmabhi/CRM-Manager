6aab8b845ec2e37e56625e36089436df8e71a717
abhinavmabhi <dexmarketing192018@gmail.com>
2025-02-04 13:06:58 +0530

    Updated project settings for Render deployment

--- build.sh
+++
@@ -1,8 +0,0 @@
-set -o errexit
-pip install -r requirments.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
-if [[ $CREATESUPERUSER ]];
-then 
-python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi
